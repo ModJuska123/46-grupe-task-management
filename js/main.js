397741a388339abe4803c15df787156394447b29
ModJuska123 <142696870+ModJuska123@users.noreply.github.com>
2023-11-06 22:56:08 +0200

    Import-export

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// LIETUVA - automobilis, sunkvezimis
+import { liekana, minus, multi, sum } from './sum.js';
+
+console.log('labas');
+
+const n1 = 7;
+const n2 = 5;
+
+
+const n12 = sum(n1, n2);
+const n12minus = minus(n1, n2);
+const n12multi = multi(n1, n2);
+const n12liekana = liekana(n1, n2);
+
+console.log(n12);
+console.log(n12minus);
+console.log(n12multi);
+console.log(n12liekana);
---
+++ js/sum.js
@@ -0,0 +1,21 @@
+function sum(a, b) {
+    return a + b;
+}
+
+function minus(a, b) {
+    return a - b;
+}
+
+function div(a, b) {
+    return a / b;
+}
+
+function multi(a, b) {
+    return a * b;
+}
+
+function liekana(a, b) {
+    return a % b;
+}
+// export default sum;
+export { sum, minus, div, multi, liekana };
