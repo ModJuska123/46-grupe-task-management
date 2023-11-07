dcdf7b04c07fad1c4362e4f10c40af3dbd225d45
ModJuska123 <142696870+ModJuska123@users.noreply.github.com>
2023-11-07 23:14:14 +0200

    Grid

--- js/main.js
+++ js/main.js
@@ -1,18 +1,25 @@
 // LIETUVA - automobilis, sunkvezimis
-import { liekana, minus, multi, sum } from './sum.js';
+// import { liekana, minus, multi, sum } from './sum.js';
 
-console.log('labas');
+// console.log('labas');
 
-const n1 = 7;
-const n2 = 5;
+// const n1 = 7;
+// const n2 = 5;
 
 
-const n12 = sum(n1, n2);
-const n12minus = minus(n1, n2);
-const n12multi = multi(n1, n2);
-const n12liekana = liekana(n1, n2);
+// const n12 = sum(n1, n2);
+// const n12minus = minus(n1, n2);
+// const n12multi = multi(n1, n2);
+// const n12liekana = liekana(n1, n2);
 
-console.log(n12);
-console.log(n12minus);
-console.log(n12multi);
-console.log(n12liekana);
+// console.log(n12);
+// console.log(n12minus);
+// console.log(n12multi);
+// console.log(n12liekana);
+
+
+import { Todo } from "./Todo.js";
+
+const kanban = new Todo('#kanban', ['Backlog', 'Todo', 'In progress', 'Done']);
+
+// kanban.addTask('');
