edc735f18a9ed70750c04c40fa51982b396aed6a
ModJuska123 <142696870+ModJuska123@users.noreply.github.com>
2023-11-12 12:38:38 +0200

    update

--- js/Todo.js
+++ js/Todo.js
@@ -1,59 +1,63 @@
-export class Todo {  //isvada-cia klases elementai
+export class Todo {  
     constructor(selector, columns) {   
-        this.selector = selector;    //selektoriu ir columns reiksmes gaus is
-        this.columns = columns;   }}   //'main.js' failo !!!!papildomi du }}!!!
-//         this.DOM = null;             // DOM ir columnsDOM priskiriamos null
-//         this.columnsDOM = [];
+        this.selector = selector;    
+        this.columns = columns;     
+        this.DOM = null;             
+        this.columnsDOM = [];
 
-//         this.init();                 //aktyvuojamas .init(...) ir veikia uz konstruktoriaus
-//     }
+        this.init();             
+    }
 
-//     init() {
-//         const err = this.updateDOMelement();
-//         if (typeof err === 'string') {
-//             return console.error(err);
-//         }
+    init() {
+        const err = this.updateDOMelement();
+        if (typeof err === 'string') {
+            return console.error(err);
+        }
 
-//         this.render();
-//     }
+        this.render();
+    }
 
-//     updateDOMelement() {
-//         if (typeof this.selector !== 'string' || this.selector === '') {
-//             return 'Netinkamas selektorius';
-//         }
+    updateDOMelement() {
+        if (typeof this.selector !== 'string' || this.selector === '') {
+            return 'Netinkamas selektorius';
+        }
 
-//         this.DOM = document.querySelector(this.selector);
-//     }
+        this.DOM = document.querySelector(this.selector);
+    }
 
-//     render() {
-//         let HTML = '';
+    render() {
+        let HTML = '';
 
-//         for (const column of this.columns) {
-//             HTML += `
-//                 <div class="column">
-//                     <h2 class="title">${column}</h2>
-//                     <ul class="task-list"></ul>
-//                 </div>`;
-//         }
+        for (const column of this.columns) {
+            HTML += `
+                <div class="column">
+                    <h2>${column}</h2>
+                    <ul>
+                        <li>Task</li>
+                        <li>Task</li>
+                        <li>Task</li>
+                        <li>Task</li>
+                        <li>Task</li>
+                    </ul>
+                </div>`;
+        }
 
-//         this.DOM.classList.add('todo'); //priseda htmlo eilutei klase
-//         this.DOM.innerHTML = HTML; //suvaro koda i HTMLa.
-//         this.DOM.style.gridTemplateColumns = `repeat(${this.columns.length}, 1fr)`;
+        this.DOM.classList.add('todo'); 
+        this.DOM.innerHTML = HTML; 
+        this.DOM.style.gridTemplateColumns = `repeat(${this.columns.length}, 1fr)`;
         
-//         this.columnsDOM = this.DOM.querySelectorAll('.task-list');
+        this.columnsDOM = this.DOM.querySelectorAll('.task-list');
         
-//         console.log('this.columnsDOM'); //tikrinam ar pavyks rasti
-
-//     }
-//     addTask(data) {
-//     const HTML = `
-//     <li class="task-card">
-//         <div>{task.title}</div>
-//         <div>{task.desc}</div>
-//         <div>{task.tag}</div>
-//         <div>{task.dedline}</div>
-//     </li>;`
-//     console.log(HTML);
-//     }
-//   }
-// 
+        console.log('this.columnsDOM'); 
+    }
+    addTask(data) {
+    const HTML = `
+    <li class="task-card">
+        <div>{task.title}</div>
+        <div>{task.desc}</div>
+        <div>{task.tag}</div>
+        <div>{task.dedline}</div>
+    </li>;`
+    console.log(HTML);
+    }
+  }
--- js/main.js
+++ js/main.js
@@ -1,31 +1,31 @@
-import { Todo } from "./Todo.js"; //importuok Todo is ./Todo.js
+import { Todo } from "./Todo.js"; 
 
 
-const kanban = new Todo('#kanban', ['Backlog', 'Todo', 'In progress', 'Done']); //uz skl selektorius #kanban
+const kanban = new Todo('#kanban', ['Backlog', 'Todo', 'In progress', 'Done']); 
+console.log(kanban);
+kanban.addTask({
+    columnIndex: 1, 
+    title: "Pirmos uzduoties antraste",
+    desc: "Pirmos uzduoties pilnas aprasas",
+    createdOn: "2023-11-08 9:03:15",
+    deadLine: "2023-12-24 0:0:0", 
+    tags: ['Design'],
+});
 
-// kanban.addTask({
-//     columnIndex: 1,
-//     title: "Pirmos uzduoties antraste",
-//     desc: "Pirmos uzduoties pilnas aprasas",
-//     createdOn: "2023-11-08 9:03:15",
-//     deadLine: "2023-12-24 0:0:0", 
-//     tags: ['Design'],
-// });
+kanban.addTask({
+    columnIndex: 0,
+    title: "Antros uzduoties antraste",
+    desc: "Pirmos uzduoties pilnas aprasas",
+    createdOn: "2023-11-08 9:03:15",
+    deadLine: "2023-12-24 0:0:0", 
+    tags: ['Design'],
+});
 
-// kanban.addTask({
-//     columnIndex: 0,
-//     title: "Antros uzduoties antraste",
-//     desc: "Pirmos uzduoties pilnas aprasas",
-//     createdOn: "2023-11-08 9:03:15",
-//     deadLine: "2023-12-24 0:0:0", 
-//     tags: ['Design'],
-// });
-
-// kanban.addTask({
-//     columnIndex: 0,
-//     title: "Trecios uzduoties antraste",
-//     desc: "Pirmos uzduoties pilnas aprasas",
-//     createdOn: "2023-11-08 9:03:15",
-//     deadLine: "2023-12-24 0:0:0", 
-//     tags: ['Design'],
-// });
+kanban.addTask({
+    columnIndex: 0,
+    title: "Trecios uzduoties antraste",
+    desc: "Pirmos uzduoties pilnas aprasas",
+    createdOn: "2023-11-08 9:03:15",
+    deadLine: "2023-12-24 0:0:0", 
+    tags: ['Design'],
+});
